5595503129341fd850f58e7d49694340fffdc338
karan jain <karanjain2212@gmail.com>
2023-02-21 22:32:51 +0530

    styled footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,10 @@
 import React from "react";
-
+import "./Footer.scss";
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="footer">
+      <div>Movie geeks</div>
+      <div>Created by karanJain @2023 </div>
+    </div>
+  );
 }

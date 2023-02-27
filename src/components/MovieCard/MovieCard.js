6b0845ba4c57753a6d5f4e829f12fa5cdacdfaa3
karan jain <karanjain2212@gmail.com>
2023-02-27 23:08:49 +0530

    Hover effect

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -1,20 +1,23 @@
 import React from "react";
 import "./MovieCard.scss";
+import { Link } from "react-router-dom";
 export default function MovieCard(props) {
   const { data } = props;
   return (
     <div className="card-item">
-      <div className="card-inner">
-        <div className="card-top">
-          <img src={data.Poster} alt={data.title} />
-        </div>
-        <div className="card-bottom">
-          <div className="card-info">
-            <h4>{data.Title}</h4>
-            <p>{data.Year}</p>
+      <Link to={`/movie/:${data.imdbID}`}>
+        <div className="card-inner">
+          <div className="card-top">
+            <img src={data.Poster} alt={data.title} />
+          </div>
+          <div className="card-bottom">
+            <div className="card-info">
+              <h4>{data.Title}</h4>
+              <p>{data.Year}</p>
+            </div>
           </div>
         </div>
-      </div>
+      </Link>
     </div>
   );
 }
